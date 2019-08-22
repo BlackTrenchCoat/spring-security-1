e7f6183b0f6c1756602b8b3a3ef16e110ffb445a
Larry Mulcahy <lmulcahy@nyx.net>
2019-08-22 04:52:31 -0600

    Add a second module for JDBC with custom DDL. Add a top-level, multi-module POM.

---
+++ spring-security-jdbc-custom-ddl/src/main/java/org/theproject/springsecurityjdbc/SpringSecurityJdbcCustomDdlApplication.java
@@ -0,0 +1,13 @@
+package org.theproject.springsecurityjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJdbcCustomDdlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityJdbcCustomDdlApplication.class, args);
+    }
+
+}
