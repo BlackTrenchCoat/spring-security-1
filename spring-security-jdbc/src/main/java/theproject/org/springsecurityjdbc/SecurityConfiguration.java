e5ab250a685cd17a5a225cf79016904995aafb5c
Larry Mulcahy <lmulcahy@nyx.net>
2019-08-21 18:48:25 -0600

    Rename the project

--- spring-security-jdbc/src/main/java/theproject/org/springsecurityjdbc/SecurityConfiguration.java
+++ spring-security-jdbc/src/main/java/theproject/org/springsecurityjdbc/SecurityConfiguration.java
@@ -1,10 +1,8 @@
 package theproject.org.springsecurityjdbc;
 
-import org.hibernate.bytecode.enhance.internal.tracker.NoopCollectionTracker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -24,14 +22,4 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .passwordEncoder(NoOpPasswordEncoder.getInstance())
                 .dataSource(dataSource);
     }
-
-/*
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin();
-    }
-*/
 }
