d99dd689d7623d0f78fbd529eb171b0fda8ebc07
Larry Mulcahy <lmulcahy@nyx.net>
2019-08-25 15:32:15 -0600

    Use an embedded H2 database for spring-security-jdbc-custom-ddl

---
+++ spring-security-jdbc-custom-ddl/src/main/java/org/theproject/springsecurityjdbc/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package org.theproject.springsecurityjdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .dataSource(dataSource);
+    }
+
+}
