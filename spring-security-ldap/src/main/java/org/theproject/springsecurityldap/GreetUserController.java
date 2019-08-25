911f5e7a6509edc80ea25eb3594412a9be86df0b
lmulcahy <larry.mulcahy@epsilon.com>
2019-08-25 13:28:47 -0600

    Greet the user by username

--- spring-security-ldap/src/main/java/org/theproject/springsecurityldap/GreetUserController.java
+++ spring-security-ldap/src/main/java/org/theproject/springsecurityldap/GreetUserController.java
@@ -1,5 +1,6 @@
 package org.theproject.springsecurityldap;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetUserController {
 
     @RequestMapping("/")
-    public String hello() {
-        return "Welcome";
+    public String hello(Authentication auth) {
+        return "Welcome " + auth.getName();
     }
 }
