9ec30952cc29ffa5a44fb42ca9429a1b98d4b905
Larry Mulcahy <lmulcahy@nyx.net>
2019-09-08 16:14:33 -0600

    Add a /principal endpoint to the controller

--- spring-security-jdbc-custom-ddl/src/main/java/org/theproject/springsecurityjdbc/GreetUserController.java
+++ spring-security-jdbc-custom-ddl/src/main/java/org/theproject/springsecurityjdbc/GreetUserController.java
@@ -1,8 +1,13 @@
 package org.theproject.springsecurityjdbc;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.stream.Collectors;
 
 @RestController
 public class GreetUserController {
@@ -10,6 +15,15 @@ public class GreetUserController {
     @RequestMapping("/")
     public String hello(Authentication auth) {
         return "Welcome " + auth.getName();
+    }
+
+    @RequestMapping("/principal")
+    public String helloPrincipal(Principal principal) {
+        return "Welcome " + principal.getName() +
+                ", your authorities are " +
+                ((UsernamePasswordAuthenticationToken) principal).getAuthorities().stream()
+                        .map(x -> x.toString())
+                        .collect(Collectors.joining(", "));
     }
 
 }
