385a8deedd03e9fbe4a6a5bc23ad1782f5380269
Larry Mulcahy <lmulcahy@nyx.net>
2019-09-08 07:23:13 -0600

    In-memory authentication

---
+++ spring-security-inmemory/src/main/java/org/theproject/springsecurityinmemory/GreetUserController.java
@@ -0,0 +1,28 @@
+package org.theproject.springsecurityinmemory;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.stream.Collectors;
+
+@RestController
+public class GreetUserController {
+
+    @RequestMapping("/")
+    public String hello(Authentication auth) {
+        return "Welcome " + auth.getName();
+    }
+
+    @RequestMapping("/principal")
+    public String helloPrincipal(Principal principal) {
+        return "Welcome " + principal.getName() +
+                ", your authorities are " +
+                ((UsernamePasswordAuthenticationToken) principal).getAuthorities().stream()
+                        .map(x -> x.toString())
+                        .collect(Collectors.joining(", "));
+    }
+
+}
